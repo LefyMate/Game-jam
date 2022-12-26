fd1e4b047cbc146def17a5756a3f15d7d27011f4
LefyMate <xklaba02@stud.fit.vutbr.cz>
2022-12-26 07:48:28 +0100

    Ricochet update
    Bullets changed to rubber insted of manually working out vector reflections

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -4,24 +4,61 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    /*
+    public float speed = 10f;
+
+    [SerializeField] private LayerMask CollisionMask;
+
+    [SerializeField] private int layer = 8;
+    //private int layerAsLayerMask;
+
+    private void Start()
+    {
+        CollisionMask |= (1 << layer);
+    }
+    */
 
 
-    void OnCollisionEnter2D(Collision2D col)
+    private Rigidbody2D rb;
+
+    Vector3 velocity;
+
+    private void Awake()
+    {
+        rb=GetComponent<Rigidbody2D>();
+    }
+
+
+    void Update()
     {
 
-        if (col.gameObject.CompareTag("Block"))
+        velocity=rb.velocity;
+
+        /*
+        transform.position +=transform.up*speed*Time.deltaTime;
+
+        Ray2D impact = new Ray2D(transform.position,transform.up);
+        RaycastHit2D collision = Physics2D.Raycast(impact.origin, impact.direction,Time.deltaTime*speed+0.2f,CollisionMask);
+        if (collision.collider!=null)
         {
-            Ray2D impact = new Ray2D(transform.position, transform.right);
-            //RaycastHit2D collision = Physics2D.Raycast(impact.origin, impact.direction, col.relativeVelocity, CollisionMask);
-
-            if (col.collider != null)
-            {
-                Vector2 bounce = Vector2.Reflect(impact.direction, col.contacts[0].normal);
-                float angle = 90 - Mathf.Atan2(bounce.y, bounce.x) * Mathf.Rad2Deg;
-                transform.eulerAngles = new Vector3(0, 0, angle);
-            }
+            Debug.DrawLine(impact.origin,collision.point,Color.blue);
+            Vector2 bounce = Vector2.Reflect(impact.direction,collision.normal);
+            float angle = 90-Mathf.Atan2(bounce.x, bounce.y) * Mathf.Rad2Deg;
+            Debug.DrawRay(collision.point,bounce,Color.red);
+            transform.eulerAngles = new Vector3(0, 0, angle);
         }
+        */
 
-        
     }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var speed = velocity.magnitude;
+        var direction = Vector3.Reflect(velocity.normalized, collision.contacts[0].normal);
+
+        rb.velocity = direction * Mathf.Max(speed, 0f);
+    }
+
+
 }
