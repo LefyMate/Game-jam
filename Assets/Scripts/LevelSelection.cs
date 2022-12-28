229cfadd85deee1da6f85c7d670d737c28024ffe
LefyMate <xklaba02@stud.fit.vutbr.cz>
2022-12-28 21:23:00 +0100

    Final changes

--- Assets/Scripts/LevelSelection.cs
+++ Assets/Scripts/LevelSelection.cs
@@ -7,10 +7,12 @@ public class LevelSelection : MonoBehaviour
 {
     public Button[] lvlButtons;
 
+    private int FirstLevelSceneIndex = 2;
+
     // Start is called before the first frame update
     void Start()
     {
-        int levelAt = PlayerPrefs.GetInt("levelAt", 2); /* < Change this int value to whatever your
+        int levelAt = PlayerPrefs.GetInt("levelAt", FirstLevelSceneIndex); /* < Change this int value to whatever your
                                                              level selection build index is on your
                                                              build settings */
 
@@ -24,7 +26,7 @@ public class LevelSelection : MonoBehaviour
     //resets progress and locks levels aside from level 1
     public void ClearProgress()
     {
-        PlayerPrefs.SetInt("levelAt", 2);
+        PlayerPrefs.SetInt("levelAt", FirstLevelSceneIndex);
     }
 
 }
